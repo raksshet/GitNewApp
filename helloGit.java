6754791e12b789ce66a53cf80afecfd0d0888625
Rakshith Shetty <rshetty89@gmail.com>
2019-05-02 15:48:17 +0000

    3rd commit on java file

--- helloGit.java
+++ helloGit.java
@@ -2,6 +2,9 @@ public class helloGit{
 /fisrt java code in linux
 public static void main(string[] arg){
 system.out.print("Hello Git");
+//second output
+//code is good now
+system.ot.println("New line to the code");
 
    }
 }
