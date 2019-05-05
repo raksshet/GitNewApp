5b1efb6fae674242671da1de5a6f3c7995c48ac2
Rakshith Shetty <rshetty89@gmail.com>
2019-05-05 02:31:18 +0000

    New line from dev2-branch

--- helloGit.java
+++ helloGit.java
@@ -2,9 +2,8 @@ public class helloGit{
 /fisrt java code in linux
 public static void main(string[] arg){
 system.out.print("Hello Git");
-//second output
 //code is good now
 system.ot.println("New line to the code");
-
+system.out.prinln(New line from brach 2");
    }
 }
